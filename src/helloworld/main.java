14f5bdd48f754a2fc2c04d11485d4e7098eaaf4c
dvwells1@gmail.com <dvwells1@gmail.com>
2018-02-01 15:26:07 -0700

    This is my initial commit for myFirstGitRepo

---
+++ src/helloworld/main.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello There");
+		System.out.println("Adding a new line to helloworld");
+
+	}
+
+}
